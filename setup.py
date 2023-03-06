bd6b36fb51fdd75743a623e301a73a94787d40af
GENIUS-926995\vivek sharma <vivek.sharma@geniussports.com>
2023-03-06 21:39:01 +0000

    setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str) -> List[str]:
+    '''
+    This  function will return the list of requirments
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove('-e .') 
+
+setup(
+name = 'mlproject',
+version = '0.0.1',
+author = 'Vivek',
+author_email = 'c4viveksharama@gmail.com',
+packages= find_packages(),
+install_requires = get_requirements('requirements.txt')
+
+)
+
